9eb114a12634281254dcc26b94885a7a08ca99e5
pk karki <prabin@abhiyantrik.com>
2019-07-18 09:20:14 +0545

    treatment api

--- treatmentapp/apiurls.py
+++ treatmentapp/apiurls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include
 from django.urls import path
 from treatmentapp.api.visualization import Visualization, Visualization1
 from rest_framework.urlpatterns import format_suffix_patterns
-
+from treatmentapp.api.treatment import PatientTreatmentView, PatientTreatmentUpdateView
 
 
 app_name = 'treatmentapp'
@@ -11,5 +11,7 @@ app_name = 'treatmentapp'
 urlpatterns = [
 	path('visualizations', Visualization.as_view()),
 	path('visualization/locations', Visualization1.as_view()),
+	path('encounter/<encounter_id>/treatment', PatientTreatmentView.as_view()),
+	path('encounter/<encounter_id>/treatment/update', PatientTreatmentUpdateView.as_view()),
     ]
 urlpatterns = format_suffix_patterns(urlpatterns)
