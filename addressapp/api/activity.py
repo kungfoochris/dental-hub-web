fe6ded8945f71282ab2037a7887e62b1e4e2d9f3
pk karki <prabin@abhiyantrik.com>
2019-07-26 13:49:05 +0545

    activitis login not required

--- addressapp/api/activity.py
+++ addressapp/api/activity.py
@@ -15,8 +15,8 @@ from rest_framework import filters
 class IsPostOrIsAuthenticated(permissions.BasePermission):        
 
     def has_permission(self, request, view):
-        # if request.method == 'GET':
-        #     return True
+        if request.method == 'GET':
+            return True
         return request.user and request.user.is_authenticated
 
 
