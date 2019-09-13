db79c3bb44f5a1b7c1509f849954ed37ea12e7e5
pk karki <prabin@abhiyantrik.com>
2019-09-13 15:13:51 +0545

    screeing treatment and history data field change

--- visualizationapp/apiurls.py
+++ visualizationapp/apiurls.py
@@ -9,8 +9,8 @@ from visualizationapp.api.loginvisualization import LoginVisualization
 from visualizationapp.api.treatmenttable import TreatmentTableVisualization
 
 from visualizationapp.api.treatmentbargraph import TreatMentBarGraph
-# from visualizationapp.api.table import TreatmentTable1Visualization, TreatmentTable2Visualization,\
-# Table3Visualization,Table4Visualization, VisualizationSetting
+from visualizationapp.api.table import TreatmentTable1Visualization, TreatmentTable2Visualization,\
+Table3Visualization,Table4Visualization, VisualizationSetting
 
 from visualizationapp.api.wardvisualization import WardVisualization1,WardTreatmentTableVisualization1,\
 WardTableVisualization2,WardSettingVisualization, WardTreatmentVisualization
@@ -23,11 +23,11 @@ urlpatterns = [
 	path('treatment',TreatmentTableVisualization.as_view()),
 	path('treatmentnargraph',TreatMentBarGraph.as_view()),
 
-	# path('table',TreatmentTable1Visualization.as_view()),
-	# path('table1', TreatmentTable2Visualization.as_view()),
-	# path('table2',Table3Visualization.as_view()),
-	# path('table3',Table4Visualization.as_view()),
-	# path('settingsgraph',VisualizationSetting.as_view()),
+	path('table',TreatmentTable1Visualization.as_view()),
+	path('table1', TreatmentTable2Visualization.as_view()),
+	path('table2',Table3Visualization.as_view()),
+	path('table3',Table4Visualization.as_view()),
+	path('settingsgraph',VisualizationSetting.as_view()),
 
 	path('wardvisualization',WardVisualization1.as_view()),
 	path('wardtablevisualization',WardTreatmentTableVisualization1.as_view()),
