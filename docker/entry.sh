6ab7ec65a9981dc4db24c9f4cd1a0332b8aa7037
pk karki <prabin@abhiyantrik.com>
2019-11-12 14:27:13 +0545

    demo

--- docker/entry.sh
+++ docker/entry.sh
@@ -3,6 +3,6 @@
 # python3 manage.py migrate
 python manage.py makemigrations
 python manage.py migrate
-gunicorn dental.wsgi:application --bind 0.0.0.0:6061
+gunicorn dental.wsgi:application --bind 0.0.0.0:6065
 # python manage.py runserver 0.0.0.0:6061
 #
