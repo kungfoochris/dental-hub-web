d4c769738ea2472ab4bfcd15fcf3d475abbcdf7e
pk karki <prabin@abhiyantrik.com>
2020-08-10 10:17:24 +0545

    More then two encounter can not crated for same patient on same date

--- encounterapp/api/encounter.py
+++ encounterapp/api/encounter.py
@@ -1,18 +1,21 @@
-import re
 from datetime import *
+import logging
+import datetime
+import pytz
+utc = pytz.UTC
+
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
 from dateutil import relativedelta
-import pytz
-utc=pytz.UTC
+
 
 from userapp.models import User, CustomUser
 from encounterapp.models import Encounter, Refer, History
 from patientapp.models import Patient
 from encounterapp.serializers.encounter import EncounterSerializer,\
-AllEncounterSerializer,EncounterUpdateSerializer
+    AllEncounterSerializer, EncounterUpdateSerializer
 from encounterapp.models.modifydelete import ModifyDelete
 
 
@@ -21,7 +24,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from addressapp.models import ActivityArea, Ward, Activity
 
-import logging
+
+
+today_encounter_date = datetime.date.today()
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
@@ -36,7 +41,7 @@ class EncounterView(APIView):
     serializer_class = EncounterSerializer
 
 
-    def get(self, request,patient_id, format=None):
+    def get(self, request, patient_id, format=None):
         if User.objects.filter(id=request.user.id):
             if Patient.objects.filter(id=patient_id):
                 if Encounter.objects.select_related('patient').filter(patient__id=patient_id).exists():
@@ -44,15 +49,18 @@ class EncounterView(APIView):
                     serializer = AllEncounterSerializer(encounter_obj, many=True,\
                         context={'request': request})
                     return Response(serializer.data)
-                return Response({"message":'encounter not added or not found'},status=400)
-            return Response({"message":"patient id does not exist."},status=400)
-        return Response({"message":"permission not allowed"},status=400)
+                return Response({"message":'encounter not added or not found'}, status=400)
+            return Response({"message":"patient id does not exist."}, status=400)
+        return Response({"message":"permission not allowed"}, status=400)
 
     def post(self, request, patient_id, format=None):
         serializer = EncounterSerializer(data=request.data,\
             context={'request': request})
         if Patient.objects.filter(id=patient_id).exists():
             patient_obj = Patient.objects.get(id=patient_id)
+            #check More then two encounter can not crated for same patient on same date.
+            if Encounter.objects.filter(patient_id=patient_id, server_date=today_encounter_date):
+                return Response({"message":"More then two encounter can not crated for same patient on same date."}, status=400)
             if serializer.is_valid():
                 activity_area_obj = serializer.validated_data['activityarea_id']
                 if Activity.objects.filter(id=activity_area_obj).exists():
@@ -65,14 +73,14 @@ class EncounterView(APIView):
                 encounter_obj.activity_area = activity_area_obj
                 encounter_obj.geography = geography_obj
                 encounter_obj.patient = patient_obj
-                encounter_obj.author = patient_obj.author
+                encounter_obj.author = request.user
                 encounter_obj.other_problem = serializer.validated_data['other_problem']
                 encounter_obj.created_at = serializer.validated_data['created_at']
                 encounter_obj.updated_by = request.user
                 encounter_obj.updated_at = serializer.validated_data['updated_at']
                 encounter_obj.save()
                 logger.info("%s %s" %("Encounter added successfully by", request.user.full_name))
-                return Response({"message":"Encounter added","id":encounter_obj.id},status=200)
+                return Response({"message":"Encounter added", "id":encounter_obj.id}, status=200)
                 # logger.info("Geography id does not exists in encounter section.")
                 # return Response({"message":"Geography id does not exists. created by="+request.user.full_name},status=400)
                 # logger.info("Activity id does not exists in encounter section.")
@@ -160,4 +168,3 @@ class EncounterUpdateView(APIView):
             return Response({'message':serializer.errors}, status=400)
         logger.info("%s %s" %("Patient id does not  exists in encounter section : ", patient_id))
         return Response({"message":"id do not match"},status=400)
-
